034d853d01e1bb0e1472a3d21d6da7dcc2b08384
spbills <spencebills0@gmail.com>
2021-01-14 15:29:55 -0500

    Added ability to update page.

--- src/views/Page.vue
+++ src/views/Page.vue
@@ -1,18 +1,25 @@
 <template>
 	<main>
         <h1>Page {{ page.title }}</h1>
-        <FieldTable :fields="page.fields"/>
+        <FieldTable :fields="pagemodule.fields"/>
+        <form class="page-form" @submit="update_page">
+            <input type="text" v-model="page.page_name">
+            <input type="text" v-model="page.page_url">
+            <input type="text" v-model="page.page_title">
+            <button type="submit">Save</button>
+        </form>
     </main>
 </template>
 
 <script>
-import { get } from "axios";
+import { get, put } from "axios";
 import FieldTable from '@/components/FieldTable'
 
 export default {
     name: "Page",
     data() {
         return {
+            pagemodule: {},
             page: {}
         }
     },
@@ -20,15 +27,32 @@ export default {
         FieldTable
     },
 	async created() {
-		let page = await this.get_page();
-		this.page = page.message;
+		let pagemodule = await this.get_page();
+        this.pagemodule = pagemodule.message;
+        
+        let page = {...pagemodule.message};
+        delete page.fields;
+        this.page = page;
 	},
 	methods: {
 		async get_page() {
             let id = this.$route.params.id;
             let page_req = await get(`/pages/${id}/modules`);
 			return page_req.data;
-		},
+        },
+        async update_page(e) {
+            e.preventDefault();
+            
+            let id = this.$route.params.id;
+            await put(`/pages/${id}`, this.page);
+        }
 	},
 };
 </script>
+
+<style scoped>
+.page-form {
+    display: flex;
+    flex-direction: column;
+}
+</style>
