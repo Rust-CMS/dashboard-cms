f08db62e3947a7b0824d1cdacfd78e021c8e4a09
spbills <spencebills0@gmail.com>
2021-01-14 15:26:24 -0500

    Added ability to update field.

--- src/views/Field.vue
+++ src/views/Field.vue
@@ -1,29 +1,58 @@
 <template>
     <main>
         <h1>Field {{ field.title }}</h1>
+        <form class="field-form" @submit="updateField">
+            <input type="text" v-model="field.title" />
+            <input type="text" v-model="field.content" />
+            <select v-model="field.page_name">
+                <option v-for="(page, i) in pages" :key="i" :value="page.page_name">{{page.page_name}}</option>
+            </select>
+            <button type="submit">Save</button>
+        </form>
     </main>
 </template>
 
 <script>
-import { get } from "axios";
+import { get, put } from "axios";
 
 export default {
     name: "Field",
     data() {
         return {
-            field: {}
+            field: {},
+            pages: []
         }
     },
     async created() {
         let field = await this.getField();
 
         this.field = field.data.message;
+
+        let pages = await this.getPages();
+
+        this.pages = pages.data.message;
     },
     methods: {
         async getField() {
             let id = this.$route.params.id;
-            return await get(`/modules/${id}`)
+            return await get(`/modules/${id}`);
+        },
+        async getPages() {
+            return await get(`/pages`);
+        },
+        async updateField(e) {
+            e.preventDefault();
+
+            let id = this.$route.params.id;
+            return await put(`/modules/${id}`, this.field);
         }
     }
 }
 </script>
+
+<style scoped>
+.field-form {
+    display: flex;
+    flex-direction: column;
+}
+</style>
