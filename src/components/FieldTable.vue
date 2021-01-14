5c0f39a32697d3c525b44e4bfd7b10662e16ded1
spbills <spencebills0@gmail.com>
2021-01-14 12:38:09 -0500

    Added field table.

---
+++ src/components/FieldTable.vue
@@ -0,0 +1,30 @@
+<template>
+    <table>
+        <thead>
+            <th>Title</th>
+            <th>Field Type</th>
+            <th>Actions</th>
+        </thead>
+        <tbody>
+            <tr v-for="(field, i) in fields" :key="i">
+                <td>{{ field.title }}</td>
+                <td>{{ field.module_type_id }}</td>
+                <td>
+                    <a :href="`/fields/${field.module_id}`">Edit</a>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</template>
+
+<script>
+export default {
+    name: "FieldTable",
+    props: {
+        fields: {
+            type: Array,
+            required: true
+        }
+    }
+}
+</script>
--- src/views/Page.vue
+++ src/views/Page.vue
@@ -1,11 +1,13 @@
 <template>
 	<main>
         <h1>Page {{ page.title }}</h1>
+        <FieldTable :fields="page.fields"/>
     </main>
 </template>
 
 <script>
 import { get } from "axios";
+import FieldTable from '@/components/FieldTable'
 
 export default {
     name: "Page",
@@ -14,6 +16,9 @@ export default {
             page: {}
         }
     },
+    components: {
+        FieldTable
+    },
 	async created() {
 		let page = await this.get_page();
 		this.page = page.message;
@@ -21,7 +26,7 @@ export default {
 	methods: {
 		async get_page() {
             let id = this.$route.params.id;
-            let page_req = await get(`/pages/${id}`);
+            let page_req = await get(`/pages/${id}/modules`);
 			return page_req.data;
 		},
 	},
