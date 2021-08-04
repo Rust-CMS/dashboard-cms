08d3532a1187f27ef862e75b5067d61a745f5506
SpBills <spencebills0@gmail.com>
2021-08-04 02:30:59 +0000

    Added auth component variables.

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,19 +2,33 @@
 	<main>
         <h1>Login</h1>
 		<form @submit="login" method="post">
-			<input type="text" placeholder="Username" />
-			<input type="text" placeholder="Password" />
+			<input v-model="username" type="text" placeholder="Username" />
+			<input v-model="password" type="text" placeholder="Password" />
             <button type="submit">LOGIN</button>
 		</form>
 	</main>
 </template>
 
 <script>
+import { post } from "axios";
+
 export default {
 	name: "Login",
+	data() {
+		return {
+			username: "",
+			password: ""
+		}
+	},
 	methods: {
 		async login(e) {
 			e.preventDefault();
+
+			await post(`/user/login`, { username, password });
+
+			this.$router.push("/");
+
+			return;
 		},
 	},
 };
